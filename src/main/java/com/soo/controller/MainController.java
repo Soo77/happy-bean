a6cc51f2c74177892a8ce1360ba98fe6eb35a02d
Soo77 <shimsh3@naver.com>
2020-09-14 12:57:05 +0900

    header footer 분리하기, app 없애기

---
+++ src/main/java/com/soo/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.soo.controller;
+
+import com.soo.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+    @Resource private MemberService memberService;
+
+    @GetMapping("index")
+    public void index() {
+    }
+
+}
